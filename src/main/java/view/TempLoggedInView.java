d249ac8e617d39e3a015419382c12ef65b7cb4e2
Frederik Brecht <frederik.brecht@gmail.com>
2024-11-09 18:49:47 -0500

    Finished the ground work for the views for signing up/ logging in.

---
+++ src/main/java/view/TempLoggedInView.java
@@ -0,0 +1,9 @@
+package view;
+
+import javax.swing.*;
+
+public class TempLoggedInView extends JPanel {
+    private JPanel panel1;
+    private JLabel labelUsername;
+    private JLabel labelIcon;
+}
