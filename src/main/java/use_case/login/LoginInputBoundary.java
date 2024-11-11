d7d11628ebd521bc2388c579b4b57d0a6f18ea2a
Karl-Michaud <karl.michaud@mail.utoronto.ca>
2024-11-10 22:07:02 -0500

    loginInputBoundary completed

--- src/main/java/use_case/login/LoginInputBoundary.java
+++ src/main/java/use_case/login/LoginInputBoundary.java
@@ -1,4 +1,20 @@
 package use_case.login;
 
+import use_case.signup.SignupInputData;
+
+/**
+ * Input boundary for actions related to logging in.
+ */
 public interface LoginInputBoundary {
+    /**
+     * Executes the login use case.
+     * @param loginInputData the input data
+     */
+    void execute(LoginInputData loginInputData);
+
+    /**
+     * Executes the switch to signed up View.
+     */
+    void switchToSignedUpView();
+
 }
