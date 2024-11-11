6b426c9299e99d09abea83b70625d7b06a97b4db
Karl-A. Michaud <karl.michaud@mail.utoronto.ca>
2024-11-10 22:22:16 -0500

    Update LoginInputData.java

--- src/main/java/use_case/login/LoginInputData.java
+++ src/main/java/use_case/login/LoginInputData.java
@@ -1,1 +1,22 @@
+package use_case.login;
 
+/**
+ * The input Data for the Login Use Case.
+ */
+public class LoginInputData {
+    private final String email;
+    private final String password;
+
+    public LoginInputData(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
