e24708b14d296d419d6c7d87013034b041edbcb1
Karl-Michaud <karl.michaud@mail.utoronto.ca>
2024-11-13 13:56:42 -0500

    Completion of the use case

--- src/main/java/use_case/club_get_members/ClubGetMembersInteractor.java
+++ src/main/java/use_case/club_get_members/ClubGetMembersInteractor.java
@@ -1,4 +1,39 @@
 package use_case.club_get_members;
 
+import java.util.Map;
+
+import entity.user.Club;
+import entity.user.Student;
+
+/**
+ * Interactor for the get members use case.
+ */
 public class ClubGetMembersInteractor {
+    private final ClubGetMembersUserDataAccessInterface getMembersDataAccessObject;
+    private final ClubGetMembersOutputBoundary getMembersPresenter;
+
+    public ClubGetMembersInteractor(ClubGetMembersUserDataAccessInterface getMembersDataAccessObject,
+                                    ClubGetMembersOutputBoundary getMembersPresenter) {
+        this.getMembersDataAccessObject = getMembersDataAccessObject;
+        this.getMembersPresenter = getMembersPresenter;
+    }
+
+    /**
+     * Executes the get members use case.
+     * @param inputData the input data.
+     */
+    public void execute(ClubGetMembersInputData inputData) {
+        final String email = inputData.getEmail();
+        if (!getMembersDataAccessObject.existsByEmail(email)) {
+            getMembersPresenter.prepareFailView(email + ": Account does not exist.");
+        }
+        else {
+            final Club club = getMembersDataAccessObject.getClub(email);
+            final Map<Integer, Student> members = getMembersDataAccessObject.getMembers(club);
+
+            final ClubGetMembersOutputData outputData = new ClubGetMembersOutputData(inputData.getEmail(), members,
+                    false);
+            getMembersPresenter.prepareSuccessView(outputData);
+        }
+    }
 }
--- src/main/java/use_case/club_get_members/ClubGetMembersUserDataAccessInterface.java
+++ src/main/java/use_case/club_get_members/ClubGetMembersUserDataAccessInterface.java
@@ -1,4 +1,33 @@
 package use_case.club_get_members;
 
+import java.util.Map;
+
+import entity.user.Club;
+import entity.user.Student;
+
+/**
+ * Data access interface for the get members use case.
+ */
 public interface ClubGetMembersUserDataAccessInterface {
+
+    /**
+     * Checks if the email matches with a club in the database.
+     * @param email of the potential club
+     * @return true if the club exists with given email
+     */
+    boolean existsByEmail(String email);
+
+    /**
+     * Returns the Clubs with given email.
+     * @param email of the club
+     * @return the club with matching email
+     */
+    Club getClub(String email);
+
+    /**
+     * Get members of a given club.
+     * @param club the Club
+     * @return the members of the given club.
+     */
+    Map<Integer, Student> getMembers(Club club);
 }
