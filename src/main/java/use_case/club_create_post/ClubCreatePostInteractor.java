8477d2db8756e5bb71cecf7139788ccbb84b7133
Karl-Michaud <karl.michaud@mail.utoronto.ca>
2024-11-13 11:48:16 -0500

    Completion of interactor and data access interface

--- src/main/java/use_case/club_create_post/ClubCreatePostInteractor.java
+++ src/main/java/use_case/club_create_post/ClubCreatePostInteractor.java
@@ -1,4 +1,41 @@
 package use_case.club_create_post;
 
-public class ClubCreatePostInteractor {
+import entity.post.Announcement;
+
+/**
+ * Club post creation use case interactor.
+ */
+public class ClubCreatePostInteractor implements ClubCreatePostInputBoundary {
+    private final ClubCreatePostUserDataAccessInterface createPostDataAccessObject;
+    private final ClubCreatePostOutputBoundary createPostPresenter;
+
+    public ClubCreatePostInteractor(ClubCreatePostUserDataAccessInterface createPostDataAccessObject,
+                                    ClubCreatePostOutputBoundary createPostPresenter) {
+        this.createPostDataAccessObject = createPostDataAccessObject;
+        this.createPostPresenter = createPostPresenter;
+    }
+
+    @Override
+    public void execute(ClubCreatePostInputData clubCreatePostInputData) {
+        final String title = clubCreatePostInputData.getTitle();
+        final String content = clubCreatePostInputData.getContent();
+
+        if (title.isEmpty()) {
+            createPostPresenter.prepareFailView("Tittle field is empty.");
+        }
+        else if (content.isEmpty()) {
+            createPostPresenter.prepareFailView("Content field is empty.");
+        }
+        else {
+            final Announcement post = new Announcement(title, content);
+            final ClubCreatePostOutputData outputData = new ClubCreatePostOutputData(
+                    post.getTitle(), post.getContent(), post.timeOfPosting(), post.dateOfPosting(), false);
+            // Save post to database
+            createPostDataAccessObject.savePost(post);
+
+            // Prepare the success view after saving the post to database
+            createPostPresenter.prepareSuccessView(outputData);
+        }
+    }
+
 }
--- src/main/java/use_case/club_create_post/ClubCreatePostUserDataAccessInterface.java
+++ src/main/java/use_case/club_create_post/ClubCreatePostUserDataAccessInterface.java
@@ -1,4 +1,14 @@
 package use_case.club_create_post;
 
+import entity.post.Post;
+
+/**
+ * Data access interface for the post creation use case.
+ */
 public interface ClubCreatePostUserDataAccessInterface {
+    /**
+     * Save post to database.
+     * @param post the post that needs to be saved
+     */
+    void savePost(Post post);
 }
