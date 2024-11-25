49a66e3c3d3c9e2d27ca100b0fa73b2044b4c297
Royangliu <royangliu@gmail.com>
2024-11-25 18:38:31 -0500

    delete unused classes in the logout use_case

--- src/main/java/use_case/logout/LogoutInteractor.java
+++ src/main/java/use_case/logout/LogoutInteractor.java
@@ -1,4 +1,20 @@
 package use_case.logout;
 
-public class LogoutInteractor {
+/**
+ * The Logout Interactor.
+ */
+public class LogoutInteractor implements LogoutInputBoundary {
+    private final LogoutDataAccessInterface logoutDataAccessObject;
+    private final LogoutOutputBoundary logoutPresenter;
+
+    public LogoutInteractor(LogoutDataAccessInterface logoutDataAccessObject,
+                                   LogoutOutputBoundary logoutPresenter) {
+        this.logoutDataAccessObject = logoutDataAccessObject;
+        this.logoutPresenter = logoutPresenter;
+    }
+
+    @Override
+    public void execute() {
+
+    }
 }
