cb032728f7444d0a6a0a03046e81b62d1f8ed336
Royangliu <royangliu@gmail.com>
2024-11-26 16:11:22 -0500

    Deletes the old club_home interface adapter package. New one we are using is club_logged_in

--- src/main/java/view/ClubHomeView.java
+++ src/main/java/view/ClubHomeView.java
@@ -6,8 +6,7 @@ import javax.swing.event.DocumentListener;
 
 import interface_adapter.club_create_post.CreatePostController;
 import interface_adapter.club_get_posts.ClubGetPostsController;
-import interface_adapter.club_home.ClubHomeController;
-import interface_adapter.club_home.ClubLoggedInViewModel;
+import interface_adapter.club_logged_in.ClubLoggedInState;
 import interface_adapter.club_logged_in.ClubLoggedInViewModel;
 import interface_adapter.club_logged_in.get_members.GetMembersController;
 import interface_adapter.club_update_desc.ClubUpdateDescController;
@@ -43,7 +42,6 @@ public class ClubHomeView extends JPanel implements PropertyChangeListener {
     private final String viewName = "club logged in";
     private final ClubLoggedInViewModel clubLoggedInViewModel;
 
-    private ClubHomeController clubHomeController;
     private CreatePostController createPostController;  // TODO might change to one to swap to the view first
     private LogoutController logoutController;
     private ClubGetPostsController clubGetPostsController;
@@ -54,12 +52,14 @@ public class ClubHomeView extends JPanel implements PropertyChangeListener {
         this.clubLoggedInViewModel = clubLoggedInViewModel;
         this.clubLoggedInViewModel.addPropertyChangeListener(this);
 
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        this.add(panelClubHome);
+
         createPostButton.addActionListener(
                 // This creates an anonymous subclass of ActionListener and instantiates it.
                 new ActionListener() {
                     public void actionPerformed(ActionEvent evt) {
                         if (evt.getSource().equals(createPostButton)) {
-
                             createPostController.switchToCreatePost(); // TODO
                         }
                     }
@@ -83,22 +83,14 @@ public class ClubHomeView extends JPanel implements PropertyChangeListener {
                 new ActionListener() {
                     public void actionPerformed(ActionEvent evt) {
                         if (evt.getSource().equals(refreshButton)) {
-                            // Executes the Logout use case.
+                            // Executes both the Get Posts and Get Members usecase to get an updated version of them
                             final ClubLoggedInState currentState = clubLoggedInViewModel.getState();
-                            clubGetPostsController.execute(currentState.getClubEmail);
-                            getMembersController.execute(currentState.getClubEmail);
+                            clubGetPostsController.execute(currentState.getEmail());
+                            getMembersController.execute(currentState.getEmail());
                         }
                     }
                 }
         );
-
-
-
-
-        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-        this.add(panelClubHome);
-
-        // TODO add action listerners to the buttons here
     }
 
     @Override
@@ -150,11 +142,6 @@ public class ClubHomeView extends JPanel implements PropertyChangeListener {
                 documentListenerHelper();
             }
         });
-    }
-
-
-    public void setClubHomeController(ClubHomeController clubHomeController) {
-        this.clubHomeController = clubHomeController;
     }
 
     public void setCreatePostController(CreatePostController createPostController) {
