a9aca9e4a8ae4300940a236bf0a2fdb2ac7fc09d
Royangliu <royangliu@gmail.com>
2024-11-26 13:25:20 -0500

    Created a class of a panel for each displayed member in ClubHomeView

---
+++ src/main/java/view/MemberPanel.java
@@ -0,0 +1,9 @@
+package view;
+
+import javax.swing.*;
+
+public class MemberPanel {
+    private JLabel memberNameLabel;
+    private JButton removeMemberButton;
+    private JPanel memberPanel;
+}
