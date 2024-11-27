15bcc23235e1a419fe64a4c46fce3cade0d78899
Royangliu <royangliu@gmail.com>
2024-11-27 00:27:44 -0500

    Made it so going between the login view and signup view clears the views before switching

--- src/main/java/interface_adapter/login/club_login/ClubLoginPresenter.java
+++ src/main/java/interface_adapter/login/club_login/ClubLoginPresenter.java
@@ -5,6 +5,7 @@ import interface_adapter.club_logged_in.ClubLoggedInState;
 import interface_adapter.club_logged_in.ClubLoggedInViewModel;
 import interface_adapter.login.LoginState;
 import interface_adapter.login.LoginViewModel;
+import interface_adapter.signup.club_signup.ClubSignupState;
 import interface_adapter.signup.club_signup.ClubSignupViewModel;
 import use_case.login.club_login.ClubLoginOutputBoundary;
 import use_case.login.club_login.ClubLoginOutputData;
@@ -60,6 +61,17 @@ public class ClubLoginPresenter implements ClubLoginOutputBoundary {
 
     @Override
     public void switchToClubSignupView() {
+        // Clears the ClubSignupState
+        final ClubSignupState state = clubSignupViewModel.getState();
+        state.setEmail("");
+        state.setPassword("");
+        state.setUsername("");
+        state.setRepeatPassword("");
+        state.setSignupError(null);
+        this.clubSignupViewModel.setState(state);
+        this.clubSignupViewModel.firePropertyChanged();
+
+        // Switches to the club signup state
         viewManagerModel.setState(clubSignupViewModel.getViewName());
         viewManagerModel.firePropertyChanged();
     }
