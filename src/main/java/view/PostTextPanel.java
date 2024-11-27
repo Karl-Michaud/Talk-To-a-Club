446913f329e3de1566289c1b4815a930cb57a794
Royangliu <royangliu@gmail.com>
2024-11-26 20:34:49 -0500

    Added PostTextPanel for displaying each post in a club. Added the logic needed for RemoveMemberPanel to work for an individual member

---
+++ src/main/java/view/PostTextPanel.java
@@ -0,0 +1,13 @@
+package view;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/**
+ * Class of a JPanel which has a Title and Body for the text of a post.
+ */
+public class PostTextPanel extends JPanel {
+    private JPanel postTextPanel;
+    private JLabel postTitleText;
+    private JLabel postBodyText;
+}
--- src/main/java/view/RemoveMemberPanel.java
+++ src/main/java/view/RemoveMemberPanel.java
@@ -1,16 +1,44 @@
 package view;
 
+import interface_adapter.club_logged_in.club_remove_member.ClubRemoveMemberController;
+
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
- * A JPanel that contains a member's name and a button to remove that member from the club.
+ * A JPanel that contains a member's email, the club they're in, and a button to remove that member from the club.
  */
-public class RemoveMemberPanel implements JPanel{
+public class RemoveMemberPanel extends JPanel {
     private JLabel memberNameLabel;
     private JButton removeMemberButton;
     private JPanel memberPanel;
+    // the club who has the student to be deleted
+    private String clubEmail;
+    // The student who gets deleted after the button press
+    private String studentUsername;
 
-    // TODO add a listener to this button that deletes the member from the club
+    private ClubRemoveMemberController removeMemberController;
+
+    public RemoveMemberPanel(ClubRemoveMemberController removeMemberController, String clubEmail,
+                             String studentUsername) {
+        this.removeMemberController = removeMemberController;
+        this.clubEmail = clubEmail;
+        this.studentUsername = studentUsername;
+
+        removeMemberButton.addActionListener(
+                // This creates an anonymous subclass of ActionListener and instantiates it.
+                new ActionListener() {
+                    public void actionPerformed(ActionEvent evt) {
+                        if (evt.getSource().equals(removeMemberButton)) {
+                            // Executes the Logout use case.
+                            removeMemberController.execute(clubEmail, studentUsername);
+                            // TODO make Karl change it so it takes in the username and not email for student
+                        }
+                    }
+                }
+        );
+    }
 }
