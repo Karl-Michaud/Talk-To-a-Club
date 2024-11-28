e101f230696883c5ef21686989f952f0ed62ac39
KabirK-05 <kabirplanes@gmail.com>
2024-11-28 16:31:13 -0500

    minor fix

--- src/test/java/use_case/student_join_club/StudentJoinClubInteractorTest.java
+++ src/test/java/use_case/student_join_club/StudentJoinClubInteractorTest.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class StudentJoinClubInteractorTest {
-
     @Test
     void successTest() {
         // Set up in-memory repositories
