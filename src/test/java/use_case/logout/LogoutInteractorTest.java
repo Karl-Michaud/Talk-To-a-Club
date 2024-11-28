491d9bf0b55c0a23bfb27f03e25fbb5500fa2e76
Royangliu <royangliu@gmail.com>
2024-11-27 22:07:54 -0500

    Finished Logout and Signup Tests

--- src/test/java/use_case/logout/LogoutInteractorTest.java
+++ src/test/java/use_case/logout/LogoutInteractorTest.java
@@ -1,4 +1,20 @@
 package use_case.logout;
 
+import org.junit.jupiter.api.Test;
+
 public class LogoutInteractorTest {
+
+    @Test
+    void successTest() {
+        // This creates a successPresenter that tests whether the test case is as we expect.
+        // Note: This use case is to just switch to the login view, so it's a bit empty
+        LogoutOutputBoundary successPresenter = new LogoutOutputBoundary() {
+            @Override
+            public void switchToLoginView() {
+                // expected to succeed
+            }
+        };
+        LogoutInputBoundary interactor = new LogoutInteractor (successPresenter);
+        interactor.execute();
+    }
 }
